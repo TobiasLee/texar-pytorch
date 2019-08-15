f9c10a0f4929589e0a31f9c4855166010521271e
LiLei <tobiaslee@ralee.cc>
2019-08-15 15:57:56 +0800

    fix bleu bug

--- examples/seq2seq_ac/utils/util.py
+++ examples/seq2seq_ac/utils/util.py
@@ -1,21 +1,30 @@
-from nltk.translate.bleu_score import sentence_bleu
+from nltk.translate.bleu_score import sentence_bleu as nltk_bleu
+from nltk.translate.bleu_score import SmoothingFunction
+
 import torch
+import numpy as np
+
+chencherry = SmoothingFunction()
 
 
 def compute_bleu(sampled_ids, target_ids, eos_token_id, device):
     """compute reward by r(y_{1:t}) = R(Y_{1:t}) - R(Y_{1:t-1})"""
     output_ids = sampled_ids.cpu()  # bsz, len
     target_ids = target_ids.cpu()  # bsz, len
-    removed = [list(t) for t in target_ids]
-    removed = [t[1:t.index(eos_token_id)] for t in removed]  # remove sos and eos
+    removed = [t.numpy() for t in target_ids]
+    # print(np.where(removed[0] == eos_token_id)[0][0])
+    removed = [t[1:np.where(t == eos_token_id)[0][0]] for t in removed]  # remove sos and eos
     bleus = []
     # sampled_ids, dtype=torch.float32, requires_grad=True)
     for i in range(1, len(output_ids[0]) + 1):  # max_len
         partial = output_ids[:, :i]
         ps = []
         for hypo, ref in zip(partial, removed):
-            sts_bleu = sentence_bleu([ref], hypo, auto_reweigh=True)
+            hypo = hypo.numpy()
+
+            sts_bleu = nltk_bleu([ref], hypo, auto_reweigh=True, smoothing_function=chencherry.method4)
             ps.append(sts_bleu)  # partial sentence bleu
+
         if i == 1 or i == len(output_ids[0]):
             # first and last, set the difference
             bleus.append(torch.Tensor(ps))  # 32
@@ -24,3 +33,9 @@ def compute_bleu(sampled_ids, target_ids, eos_token_id, device):
     bleus = torch.stack(bleus, dim=0)
 
     return bleus.transpose(1, 0).contiguous().to(device)
+
+
+if __name__ == '__main__':
+    target = torch.Tensor([[0, 1, 1, 2, 2, 4], [0, 1, 2, 3, 2, 4]])
+    output = torch.Tensor([[1, 13, 2, 2], [1, 2, 3, 2]])
+    print(compute_bleu(sampled_ids=output, target_ids=target, eos_token_id=4, device='cpu'))
